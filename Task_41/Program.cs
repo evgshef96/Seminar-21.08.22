1e0c76cce4c9cc74a3f3a0f6451b7e74d4258f1e
Evgeny <jeka-291@mail.ru>
2022-08-22 09:32:38 +0300

    Семинар 21.08.22

---
+++ Task_41/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int[] NewArray(int[] array,int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите значение элемента массива:");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+
+    return array;
+}
+
+int PositiveNumber(int[] array,int size)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if(array[i] > 0)
+            count ++;
+    }
+
+    return count;
+}
+
+Console.WriteLine("Введите количество элементов в массиве: ");
+int size = int.Parse(Console.ReadLine()!);
+int[] array = new int[size];
+NewArray(array,size);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+int pos = PositiveNumber(array,size);
+Console.WriteLine("Количество чисел больше 0: " + pos);
+
---
+++ Task_43/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+
+string CrossingPoint(double b1,double k1,double b2,double k2)
+{
+    double subtractionB = b1 - b2;
+    double subtractionK = k1 - k2;
+    double x = (-1 * subtractionB / subtractionK);
+    double y = k2 * x + b2;
+
+    return $"({x};{y})";
+}
+
+ Console.WriteLine("Введите значение b1:");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение k1:");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение b2:");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение k2:");
+double k2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine(CrossingPoint(b1,k1,b2,k2));
